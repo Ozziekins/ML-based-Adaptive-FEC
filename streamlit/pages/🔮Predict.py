98bd0a05cec6e63358422959ce17ffded990dec0
isholasomuyiwa <isholasomuyiwa@gmail.com>
2023-05-08 22:31:18 +0300

    Update with template for predict and train

--- streamlit/pages/🔮Predict.py
+++ streamlit/pages/🔮Predict.py
@@ -1,6 +1,33 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 import time
 
 st.title(
-    """Making Predicrtion"""
+    """Making Prediction"""
 )
+
+df = pd.DataFrame(
+   np.random.randn(30, 5),
+   columns=['dropped_frames','FPS','bitrate','RTT', 'loss_rate'])
+
+st.experimental_data_editor(df, use_container_width=True)
+
+def on_click():
+    with st.spinner('Getting Predictions'):
+        time.sleep(5)     
+    st.header("Predictions")
+    st.dataframe(get_prediction(), use_container_width=True)
+    st.success("Succesfully fetched predictions")
+
+st.button("Get Prediction", on_click=on_click)
+
+
+def get_prediction():
+    return pd.DataFrame(
+        data={
+            'Predicted loss rate': np.random.randn(30),
+        },
+    )
+
+
--- streamlit/pages/🚆Train.py
+++ streamlit/pages/🚆Train.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import time
 import pandas as pd
+import uuid
+
 st.title(
     """Training Loop"""
 )
@@ -12,12 +14,12 @@ uploaded_file = st.file_uploader("Choose a file",
 
 
 def on_click():
-    with st.spinner('Preprocessing Dataset'):
-        time.sleep(5)
-    with st.spinner('Updating Clusters'):
-        time.sleep(5)
-    with st.spinner('Training Regression Module'):
-        time.sleep(5)
+    with st.spinner('Uploading dataset to the api'):
+        if 'intraining' not in st.session_state:
+            st.session_state['intraining'] = uuid.uuid4()
+            time.sleep(5)
+        else:
+            st.error("A Training Process is still occurring")
 
 
 st.button("Start Training", on_click=on_click)
@@ -28,11 +30,11 @@ st.header("Metrics")
 def get_metrics():
     return pd.DataFrame(
         data={
-            'description': ['Lorem Ipsum do ', 'Lorem ipsum do', 'Lorem ipsum do', 'Lorem ipsum do', ''],        
+            'description': ['Lorem Ipsum do ', 'Lorem ipsum do', 'Lorem ipsum do', 'Lorem ipsum do', ''],
             'Values': [0, 1, 2, 3, 4],
         },
         index=['Metric 1', 'Metric 2', 'Metric 3', 'Metric 4', 'Metric 5']
     )
 
 
-st.dataframe(get_metrics())
+st.dataframe(get_metrics(), use_container_width=True)
