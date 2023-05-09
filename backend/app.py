3ae49fee4bcc8cdb5781120887954be1b541e013
Pain122 <42460516+Pain122@users.noreply.github.com>
2023-05-10 01:35:32 +0300

    MLSevice v1.0

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
+import json
+
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI
 from fastapi import File, UploadFile
 import os
 
+from backend.models.MLService import MLService
+
 app = FastAPI()
+
+service = MLService()
 
 
 @app.post("/train")
@@ -17,15 +25,27 @@ def train(file: UploadFile = File(...)):
     finally:
         file.file.close()
 
-    return {"message": f"Successfully uploaded {file.filename}"}
+    dataset = pd.read_csv(path)
+    service.train(dataset)
+
+
+    return {"message": f"Models successfully trained!"}
 
 
 @app.post("/predict")
 def predict(file: UploadFile = File(...)):
     try:
         contents = file.file.read()
-        path = os.path.join("tmp", "predict.csv")
+        path = os.path.join("tmp", "predict.npy")
         with open(path, 'wb+') as f:
             f.write(contents)
     except Exception:
         return {"message": "There was an error getting the rows for prediction"}
+
+    try:
+        dataset = np.fromfile(path)
+        data = service.predict(dataset)
+        return json.dumps(data.tolist())
+    except Exception:
+        return {"message": "Models should be trained before making prediction!"}
+
--- backend/models/MLService.py
+++ backend/models/MLService.py
@@ -1,6 +1,8 @@
 import torch
 from sklearn.cluster import KMeans
+import numpy as np
 
+from backend.models import NUM_CLUSTERS
 from backend.models.Clustering.train import train_ae
 from backend.models.Preprocessing import Preprocessor
 from backend.models.Regression.train import train_regressors
@@ -13,22 +15,35 @@ class MLService:
         self.AE = ae
         self.regressors = regressors
         self.kmeans = kmeans
+        self.preprocessor = Preprocessor()
 
     def train(self, data):
         # Preprocessing data
-        x, y = Preprocessor().preprocess(data)
+        x, y = self.preprocessor.preprocess(data)
         x, y = torch.from_numpy(x).float(), torch.from_numpy(y).float()
         # Preparing AutoEncoder
         ae = train_ae(x)
         embeddings = ae(x).cpu().detach().numpy()
         # Preparing clusters
-        kmeans = KMeans(n_clusters=3, n_init='auto')
+        kmeans = KMeans(n_clusters=NUM_CLUSTERS, n_init='auto')
         labels = kmeans.fit_predict(embeddings)
         regressors = train_regressors(x, y, labels)
         # Rewriting the variables, so that we would not corrupt predict while training
         self.AE, self.kmeans, self.regressors = ae, kmeans, regressors
 
-    def predict(self, x):
-        embedding = self.AE(x).cpu().detach().numpy()
-        label = self.kmeans.predoct(embedding)
-        return self.regressors[label](x).cpu().detach().numpy()
+    def predict(self, data):
+        if self.AE:
+            x = self.preprocessor.preprocess_predict(data)
+
+            def _predict(i, clusters):
+                subset = [index for index, label in enumerate(clusters) if label == i]
+                return self.regressors[i](x[subset]).cpu().detach().numpy()
+
+            embedding = self.AE(x).cpu().detach().numpy()
+            labels = self.kmeans.predict(embedding)
+
+            res = [_predict(i, labels) for i in range(NUM_CLUSTERS)]
+
+            return np.vstack(res)
+        else:
+            raise Exception("Should be trained first!")
