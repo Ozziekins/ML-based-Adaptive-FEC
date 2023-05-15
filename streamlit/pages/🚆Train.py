c85c0de6f54018fc8fb1c6db75f0dba24fc6631e
Ozziekins <o.okonicha@innopolis.university>
2023-05-15 13:05:50 +0300

    removed addiional tab

--- streamlit/pages/🚆Train.py
+++ streamlit/pages/🚆Train.py
@@ -6,39 +6,39 @@ st.title(
     """Train 🚆"""
 )
 
-tab1, tab2 = st.tabs(['Train',"Metrics"])
+# tab1 = st.tabs(['Train'])
 
-with tab1:
-    st.write("In this module, you can pass files and retrain our base model to suit your needs. Currently accepts only csv files.")
-    uploaded_file = st.file_uploader("Choose a file", type=['csv'])
-
-
-    def on_click():
-        if uploaded_file is not None:
-            with st.spinner('Uploading dataset to the api'):
-                url = 'http://127.0.0.1:8000/train'
-                resp = requests.post(url=url, files={'file': uploaded_file})
-                if(resp.ok):
-                    st.success("Successfully trained files")
-                else:
-                    st.error("Error training files")
-                print(resp.content)
-        else:
-            st.error("File is empty")
+# with tab1:
+st.write("In this module, you can pass files and retrain our base model to suit your needs. Currently accepts only csv files.")
+uploaded_file = st.file_uploader("Choose a file", type=['csv'])
 
 
-    st.button("Start Training", on_click=on_click)
+def on_click():
+    if uploaded_file is not None:
+        with st.spinner('Uploading dataset to the api'):
+            url = 'http://127.0.0.1:8000/train'
+            resp = requests.post(url=url, files={'file': uploaded_file})
+            if(resp.ok):
+                st.success("Successfully trained files")
+            else:
+                st.error("Error training files")
+            print(resp.content)
+    else:
+        st.error("File is empty")
 
-with tab2:
 
-    def get_metrics():
-        return pd.DataFrame(
-            data={
-                'description': ['Lorem Ipsum do ', 'Lorem ipsum do', 'Lorem ipsum do', 'Lorem ipsum do', ''],
-                'Values': [0, 1, 2, 3, 4],
-            },
-            index=['Metric 1', 'Metric 2', 'Metric 3', 'Metric 4', 'Metric 5']
-        )
+st.button("Start Training", on_click=on_click)
+
+# with tab2:
+
+#     def get_metrics():
+#         return pd.DataFrame(
+#             data={
+#                 'description': ['Lorem Ipsum do ', 'Lorem ipsum do', 'Lorem ipsum do', 'Lorem ipsum do', ''],
+#                 'Values': [0, 1, 2, 3, 4],
+#             },
+#             index=['Metric 1', 'Metric 2', 'Metric 3', 'Metric 4', 'Metric 5']
+#         )
 
 
-    st.dataframe(get_metrics(), use_container_width=True)
+#     st.dataframe(get_metrics(), use_container_width=True)
